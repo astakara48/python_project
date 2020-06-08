5cc47b74c83d1aa7b626234add1519f169b2866f
bbyu <astakara48@gmail.com>
2020-06-08 17:44:12 +0900

    Category Classification

---
+++ category_classification.py
@@ -0,0 +1,22 @@
+import numpy as np
+from konlpy.tag import Okt
+import pickle
+import os
+
+def category_classification(content) :
+    # data : .xlsx
+    with open('vocab_list.pkl', 'wb') as f :
+        vocab_list = pickle.load(f)
+        
+    file_list = os.listdir('./TFIDF/')
+    category = [file_list[i][:-5] for i in range(len(file_list))]
+    
+    okt = Okt()
+
+    cnt_num = np.zeros(len(file_list))
+    noun_list = okt.nouns(content)
+    for noun in noun_list :
+        for j in range(3) :
+            if noun in vocab_list[j] :
+                cnt_num[j] += 1
+    return category[np.argmax(cnt_num)]
